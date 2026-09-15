fcdf76adcf6d5058a88b11a631df560de4c31bb8
Turgay Celen <turgaycelen03@gmail.com>
2026-09-15 13:10:26 +0200

    Guncelleme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import { WhatsNewPopup } from '@/components/whats-new-popup';
-import { SiteStatsSummary } from '@/components/site-stats-summary';
 import { HomeNewsClippings } from '@/components/home-news-clippings';
 import { HomeLiveCommunityUpdates } from '@/components/home-live-community-updates';
 import { HomeCompactCards } from '@/components/home-compact-cards';
@@ -23,13 +22,6 @@ export default function Home() {
 
       {/* 4. Kategori Navigasyonu (Meslekler ve Rehberler Grid) */}
       <HomeCategoryNav />
-
-      {/* 5. Site İstatistikleri */}
-      <section className="py-8 bg-slate-50 dark:bg-slate-950 border-t border-slate-200 dark:border-slate-800">
-        <div className="container mx-auto px-4 max-w-7xl">
-          <SiteStatsSummary compact={true} />
-        </div>
-      </section>
     </div>
   );
 }
